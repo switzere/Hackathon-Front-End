1a42c1c42ea2c6b09bd910df5ba14903753ee820
Kaylee <KayleeBigelow@gmail.com>
2020-08-09 11:19:00 -0400

    table and other shit work

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,20 +5,27 @@ import { VectorMap } from "react-jvectormap";
 import MultiSelect from "react-multi-select-component";
 import { jsx } from "@emotion/core";
 import styled from "@emotion/styled";
+import { Checkbox } from "semantic-ui-react";
+import Table from "./Table.js";
 
 import axios from "axios";
 
 const { getName } = require("country-list");
+const { getCode } = require("country-list");
 
 class Map extends React.Component {
   state = {
     countriesCodesArray: [],
     countriesNamesArray: [],
+    columnHeader: ["Country", "Conversion"],
     countries: [],
+    tableData: [],
     data: {},
     title: "",
     titleSet: false,
     color: "#48aeef",
+    checked: [],
+    namesToSend: [],
     selectedOptions: [],
     options: [
       { label: "Canada", value: "CA" },
@@ -35,9 +42,12 @@ class Map extends React.Component {
     axios
       .get("http://localhost:5000/requests/getCountries/")
       .then((res) => {
-        const countries = res;
+        const countries = res.data.countries;
+        const countriesNamesArray = res.data.countries;
+        console.log(res.data.countries);
         console.log(res);
         this.setState({ countries });
+        this.setState({ countriesNamesArray });
       })
       .catch((error) => {
         console.log(error);
@@ -107,29 +117,89 @@ class Map extends React.Component {
       options,
       countriesCodesArray,
       countriesNamesArray,
+      columnHeader,
+      countries,
+      namesToSend,
       color,
       titleSet,
+      checked,
+      tableData,
     } = this.state;
 
     const handleClickGo = () => {
-      async function refreshMap() {
-        //console.log(getMapObject().selected);
+      var i;
+      for (i = 0; i < checked.length; i++) {
+        namesToSend[i] = countries[checked[i]];
       }
-      refreshMap();
+      console.log(namesToSend);
+      axios
+        .post("http://localhost:5000/requests/getCountryInfo/", { namesToSend })
+        .then((res) => {
+          const tableData = res.data.countries;
+          this.setState({ tableData });
+
+          console.log(getCode(tableData[0][0].country));
+        })
+        .catch((error) => {
+          console.log(error);
+        });
     };
 
     const { getName } = require("country-list");
+
+    const onRadioChange = (e) => {
+      // current array of options
+      const checked = this.state.checked;
+      let index;
+
+      // check if the check box is checked or unchecked
+      if (e.target.checked) {
+        // add the numerical value of the checkbox to options array
+        checked.push(+e.target.value);
+      } else {
+        // or remove the value from the unchecked checkbox from the array
+        index = checked.indexOf(+e.target.value);
+        checked.splice(index, 1);
+      }
+
+      // update the state with the new array of options
+      this.setState({ checked: checked });
+      console.log(checked);
+    };
+
+    const generateHeader = () => {
+      let row = [];
+      for (var i = 0; i < columnHeader.length; i++) {
+        row.push(<th id={columnHeader[i]}>{columnHeader[i]}</th>);
+      }
+      return row;
+    };
+
+    const generateTableData = () => {
+      let rows = [];
+      for (var i = 0; i < tableData.length; i++) {
+        rows.push(
+          <tr key={i}>
+            <td key={tableData[i][0].country}>{tableData[i][0].country}</td>
+            <td key={tableData[i][0].conversion}>
+              {tableData[i][0].conversion}
+            </td>
+          </tr>
+        );
+      }
+      return rows;
+    };
 
     return (
       <div>
         <div class="center">
           <h1>Visa free travel</h1>
         </div>
-        <table class="floatingTable">
-          <tr>
-            <th>Country</th>
-            <th>Conversion</th>
-          </tr>
+        <table className="floatingTable">
+          <thread>
+            <tr>{generateHeader()}</tr>
+          </thread>
+          <tbody>{generateTableData()}</tbody>
         </table>
         <div id="whatIsSelected"></div>
         {/* <MultiSelect
@@ -137,6 +207,19 @@ class Map extends React.Component {
           options={options}
           onChange={this.onChange}
         /> */}
+        {this.state.countries.map((countries, i) => {
+          return (
+            <label key={countries[this.props.id]}>
+              <input
+                type="checkbox"
+                value={i}
+                onChange={onRadioChange.bind(this)}
+              />
+              {countries}
+            </label>
+          );
+        })}
+
         <button onClick={handleClickGo}>GO</button>
 
         <VectorMap
@@ -147,7 +230,7 @@ class Map extends React.Component {
             width: "100%",
             height: "520px",
           }}
-          onRegionClick={this.handleClick1} //gets the country code
+          //onRegionClick={this.handleClick1} //gets the country code
           containerClassName="map"
           regionStyle={{
             initial: {
--- node/routes/requests.js
+++ node/routes/requests.js
@@ -2,7 +2,30 @@ const router = require("express").Router();
 
 router.route("/getCountries").get((req, res) => {
   console.log("getcountries");
-  res.json({ ok: "ok" });
+  res.json({
+    countries: [
+      "Canada",
+      "Japan",
+      "Korea",
+      "United States",
+      "European Union",
+      "Mexico",
+    ],
+  });
+  //.catch((err) => res.status(400).json("Error: " + err));
+});
+
+router.route("/getCountryInfo").post((req, res) => {
+  console.log("getCountryInfo");
+  res.json({
+    countries: [
+      { country: "Canada", conversion: "1.13" },
+      { country: "Japan", conversion: "2.1" },
+      { country: "Korea", conversion: "??" },
+      { country: "United States", conversion: "1.43" },
+      { country: "European Union", conversion: ".84" },
+    ],
+  });
   //.catch((err) => res.status(400).json("Error: " + err));
 });
 
